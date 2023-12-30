cde7329e233ee0ab85ddb14a146aa1dd5e9956ed
balazodeldiablo <63554909+balazodeldiablo@users.noreply.github.com>
2023-12-30 23:25:44 +0000

    HW5 - My database is connected to my React App. Replit is been having issues with "502 Bad Gateway" lately. Otherwise, everything else looks great. Thank you, professor.
    HW5

--- index.js
+++ index.js
@@ -1,120 +1,132 @@
-'use strict';
-import express from 'express';
-import { Car } from './models/Car.js';
-import cors from 'cors';
+"use strict";
+import express from "express";
+import { Car } from "./models/Car.js";
+import cors from "cors";
 
 
 const app = express();
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 app.use(express.static("public"));
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(express.json());
-app.use('/api', cors());
+app.use("/api", cors());
 
 
-app.listen(app.get('port'), () => {
-  console.log('Express started'); 
+app.listen(app.get("port"), () => {
+  console.log("Express started");
 });
 
 
-app.get('/', (req, res, next) => {
-  Car.find({}).lean()
+app.get("/", (req, res) => {
+
+  Car.find({})
+    .lean()
     .then((cars) => {
-      res.render('home', { cars });
+      res.render("home", {cars: JSON.stringify(cars)});
     })
-    .catch(err => next(err))
+    .catch((err) => {
+      res.status(500).send("Database Error occurred");
+    });
+  
 });
 
 
-app.get('/cars', (req,res,next) => {
-  Car.findOne({ _id:req.query._id }).lean()
-      .then((car) => {
-        res.render('detail', { car });
-      })
-      .catch(err => next(err));
+app.get("/:_id", (req, res, next) => {
+  
+  Car.findOne({ _id: req.params._id })
+    .lean()
+    .then((car) => {
+      res.render("detail", { car });
+    })
+    .catch((err) => next(err));
+  
 });
 
 
-app.get('/api/cars', (req,res) => {
-    Car.find({}).lean()
-      .then((cars) => {
-        res.json(cars);
-      })
-      .catch(err =>  {
-        res.status(500).send('Database Error occurred');
-      })
+app.get("/cars", (req, res, next) => {
+  Car.findOne({ _id: req.query._id })
+    .lean()
+    .then((car) => {
+      res.render("detail", { car });
+    })
+    .catch((err) => next(err));
+});
+
+app.get("/api/cars", (req, res) => {
+  Car.find({})
+    .lean()
+    .then((cars) => {
+      res.json(cars);
+    })
+    .catch((err) => {
+      res.status(500).send("Database Error occurred");
+    });
+});
+
+app.get("/api/cars/:_id", (req, res) => {
+  Car.findOne({ _id: req.params._id })
+    .lean()
+    .then((cars) => {
+      res.json(cars);
+    })
+    .catch((err) => {
+      res.status(500).send("Database Error occurred");
+    });
+});
+
+app.get("/about", (req, res) => {
+      res.render("about");
 });
 
 
-app.get('/api/cars/:_id', (req,res) => {
-    Car.findOne({ _id:req.params._id }).lean()
-        .then((cars) => {
-           res.json(cars);
-        })
-        .catch(err => {
-            res.status(500).send('Database Error occurred');
-        });
+app.post("/api/add/", (req, res, next) => {
+  if (!req.body._id) {
+    // This method was chosen since a Mongoose model doesn't support "insertOne()" or "insert()" methods. I downgraded my Mongoose version to 6.10.0. so the syntax you provided in the video could work.
+    Car.create(
+      {
+        make: req.body.make,
+        model: req.body.model,
+        year: req.body.year,
+        engine: req.body.engine,
+        miles: req.body.miles,
+      },
+      (err, result) => {
+        if (err) return next(err);
+        res.json({ added: 1, _id: result._id });
+      },
+    );
+  } else {
+    Car.updateOne(
+      {
+        _id: req.body._id,
+      },
+
+      {
+        make: req.body.make,
+        model: req.body.model,
+        year: req.body.year,
+        engine: req.body.engine,
+        miles: req.body.miles,
+        id: req.body.id,
+      },
+      (err, result) => {
+        if (err) return next(err);
+        res.json({ updated: result.modifiedCount, _id: req.body._id });
+      },
+    );
+  }
 });
 
-
-app.get('/about', (req,res) => {
-  res.type('text/html');
-  res.sendFile('about.html', {root: './public/html'});
-});
-
-
-app.post('/api/add/', (req,res, next) => {
-
-    if (!req.body._id) {
-    // This method was chosen since a Mongoose model doesn't support "insertOne()" or "insert()" methods. I downgraded my Mongoose version to 6.10.0. so the syntax you provided in the video could work.  
-      Car.create(
-        {
-          make: req.body.make, 
-          model: req.body.model, 
-          year: req.body.year, 
-          engine: req.body.engine, 
-          miles: req.body.miles
-        }, (err, result) => {
-          
-          if (err) return next(err);
-          res.json({added: 1, _id: result._id});
-          
-        });
-      
-    } else {
-    
-      Car.updateOne(
-        {
-          _id: req.body._id
-        }, 
-        
-        {
-          make: req.body.make, 
-          model: req.body.model, 
-          year: req.body.year, 
-          engine: req.body.engine, 
-          miles: req.body.miles, 
-          id: req.body.id
-        }, (err, result) => {
-          
-          if (err) return next(err);
-          res.json({updated:result.modifiedCount, _id: req.body._id});
-          
-        });
-      
-    }
-});
-
-app.delete('/api/delete/:_id', (req,res, next) => {
-  Car.deleteOne({_id: req.params._id}, (err, result) => {
+app.delete("/api/delete/:_id", (req, res, next) => {
+  Car.deleteOne({ _id: req.params._id }, (err, result) => {
     if (err) return next(err);
     console.log(result);
-    res.json({deleted: result.deletedCount, _id: req.params._id});
+    res.json({ deleted: result.deletedCount, _id: req.params._id });
   });
 });
 
-app.use((req,res) => {
- res.type('text/plain'); 
- res.status(404);
- res.send('404 - Not found');
+app.use((req, res) => {
+  res.type("text/plain");
+  res.status(404);
+  res.send("404 - Not found");
 });
--- models/Car.js
+++ models/Car.js
@@ -1,23 +1,21 @@
-import mongoose from 'mongoose';
-import { connectionString } from '../credentials.js';
+import mongoose from "mongoose";
+import { connectionString } from "../credentials.js";
 const { Schema } = mongoose;
 
-
 mongoose.connect(connectionString, {
-    dbName: 'IT122',
+  dbName: "IT122",
 });
 
-mongoose.connection.on('open', () => {
-  console.log('Mongoose connected.');
+mongoose.connection.on("open", () => {
+  console.log("Mongoose connected.");
 });
-
 
 const carSchema = new Schema({
-  make : { type: String, required: true },
-  model : { type: String, required: true },
-  year : { type: Number, required: true },
-  engine : String,
-  miles: Number
+  make: { type: String, required: true },
+  model: { type: String, required: true },
+  year: { type: Number, required: true },
+  engine: String,
+  miles: Number,
 });
 
-export const Car = mongoose.model('Car', carSchema);
+export const Car = mongoose.model("Car", carSchema);
